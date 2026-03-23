553a4f8e01db63c1124bffd04d5deb4b4de6849a
tvoibambuk <rgingerfraue@gmail.com>
2026-03-23 19:55:06 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+BOT_TOKEN = "8341059407:AAHNEtZ1g2iLhlgGU6MzvxT--50yJ28gF8U"  # Замени на свой токен
+ADMIN_IDS = [123456789]  # ID админов для ручного добавления матчей
+
+# Сумма ежедневного бонуса
+DAILY_BONUS = 1000
+
+# Путь к базе данных
+DB_PATH = "database.db"
